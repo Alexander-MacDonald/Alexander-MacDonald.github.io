3a45b03f55c564caadcef93d6b8118b40c93d782
alex <aemacdonald@wpi.edu>
2022-09-21 19:14:08 -0400

    added mobile detection support for S22 Ultra

--- Scripts/index-mobile.js
+++ Scripts/index-mobile.js
@@ -1,7 +1,7 @@
 window.onresize = (event) => {
     var width = document.documentElement.clientWidth;
 
-    if (width >= 1300) {
+    if (width >= 1500) {
         window.location.href = "index.html";
     }
 }
--- Scripts/index.js
+++ Scripts/index.js
@@ -2,7 +2,7 @@ window.onload = (event) => {
 
     var width = document.documentElement.clientWidth;
 
-    if (width < 1300) {
+    if (width < 1500) {
         window.location.href = "index-mobile.html";
     }
 }
